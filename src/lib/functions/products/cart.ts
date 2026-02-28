a5429158638f68f893cc3747be0999db18cc81c1
Guilherme Buys <guibuys55@gmail.com>
2026-02-28 15:20:59 -0300

    feat: creating the essential functions

---
+++ src/lib/functions/products/cart.ts
@@ -0,0 +1,9 @@
+function addToCart() {}
+
+function removeFromCart() {}
+
+function addItemAmount() {}
+
+function applyCoupon() {}
+
+export { addItemAmount, removeFromCart, addToCart, applyCoupon }
